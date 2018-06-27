a8dbc97b1fb35758f85e3eae6a01e437c1f072f4
Hollie <moonkatcreations@gmail.com>
2018-06-26 21:09:21 -0500

    Some more UI updates
    To Home.vue, App.vue, GlobalNav.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,99 +1,5 @@
 <template>
   <v-app id="inspire">
-    <!--
-    <v-navigation-drawer
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout
-            v-if="item.heading"
-            :key="item.heading"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click=""
-            >
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else :key="item.text" :to="item.link">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    -->
-    <v-toolbar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      app
-      fixed
-    >
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">MyNewPath</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down" v-if="!loggedIn">
-        <v-btn flat @click="nav('/login')">
-          <router-link to="/login" class="white--text">
-            Login
-          </router-link>
-        </v-btn>
-        <v-btn flat @click="nav('/signup')">
-          <router-link to="/signup" class="white--text">
-            Signup
-          </router-link>
-        </v-btn>
-      </v-toolbar-items>
-      <v-toolbar-items class="hidden-sm-and-down" v-else>
-        <v-btn flat @click="nav('/logout')">
-          <router-link to="/logout" class="white--text">
-            Logout
-          </router-link>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
         <v-layout justify-center>
@@ -191,6 +97,7 @@
     background-color: #ebf3e9 !important;
   }
   .container {
-    padding-top: 0 !important;
+    padding: 0 !important;
+    max-width: 100% !important;
   }
 </style>
